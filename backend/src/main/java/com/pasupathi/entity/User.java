7bec4b09c74a74a70515fe76aed86dd2d77ec620
sword7278 <sword7278@gmail.com>
2025-11-27 18:10:52 +0530

    Add files via upload

---
+++ backend/src/main/java/com/pasupathi/entity/User.java
@@ -0,0 +1,40 @@
+package com.pasupathi.TasksApp.entity;
+
+import com.pasupathi.TasksApp.enums.Role;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Task> tasks = new ArrayList<>();
+}
