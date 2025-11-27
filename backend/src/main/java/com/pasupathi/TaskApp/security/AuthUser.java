769d99c772bdf48fd80b29b1efa732da0f4c1af1
sword7278 <sword7278@gmail.com>
2025-11-27 18:35:43 +0530

    Add files via upload

---
+++ backend/src/main/java/com/pasupathi/TaskApp/security/AuthUser.java
@@ -0,0 +1,34 @@
+package com.pasupathi.TasksApp.security;
+
+
+import com.pasupathi.TasksApp.entity.User;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Builder
+@Data
+public class AuthUser implements UserDetails {
+
+    private User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(user.getRole().name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+}
