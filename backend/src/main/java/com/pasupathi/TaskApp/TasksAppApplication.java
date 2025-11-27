5980e1c0c15665f45659ef3c85c8f0cdfeda4868
sword7278 <sword7278@gmail.com>
2025-11-27 18:38:00 +0530

    Add files via upload

---
+++ backend/src/main/java/com/pasupathi/TaskApp/TasksAppApplication.java
@@ -0,0 +1,13 @@
+package com.pasupathi.TasksApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasksAppApplication.class, args);
+	}
+
+}
